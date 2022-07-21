129b1982875be7dc35a8d3f6f4e5373053561786
bmzhong <508304482@qq.com>
2022-07-21 21:40:14 +0800

    init 1

--- code/code1/test.py
+++ code/code1/test.py
@@ -1,7 +1,7 @@
 import numpy as np
 import scanpy as sc
 import pandas as pd
-adult_wc_path = '../results/adult_wc_T.csv'
+adult_wc_path = '../../results/logs1/adult_wc_T.csv'
 adult_wc = sc.read_csv(adult_wc_path)
 # adult_wc.write_csvs('../results/adult_wc/')
 sc.pp.neighbors(adult_wc, n_neighbors=10, n_pcs=40)
