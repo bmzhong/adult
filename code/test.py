d9e18e5b4ea6abed4dc1b4dcc06f49dc1638f56b
bmzhong <508304482@qq.com>
2022-07-21 21:24:44 +0800

    init

---
+++ code/test.py
@@ -0,0 +1,13 @@
+import numpy as np
+import scanpy as sc
+import pandas as pd
+adult_wc_path = '../results/adult_wc_T.csv'
+adult_wc = sc.read_csv(adult_wc_path)
+# adult_wc.write_csvs('../results/adult_wc/')
+sc.pp.neighbors(adult_wc, n_neighbors=10, n_pcs=40)
+sc.tl.umap(adult_wc)
+sc.tl.tsne(adult_wc)
+# sc.pl.umap(adult_wc,color='mt.Rnr2')
+sc.tl.leiden(adult_wc)
+sc.pl.umap(adult_wc, color=['leiden'])
+sc.pl.tsne(adult_wc, color=['leiden'])
